95fc28347f0da8e4d76ecbfb83167a1222faa985
alitafreshi <alitafreshi123@yahoo.com>
2023-04-17 09:58:02 +0430

    add Repository annotation for the note repository

--- src/main/kotlin/com/alitafreshi/note_app/repository/NoteRepository.kt
+++ src/main/kotlin/com/alitafreshi/note_app/repository/NoteRepository.kt
@@ -2,6 +2,8 @@ package com.alitafreshi.note_app.repository
 
 import com.alitafreshi.note_app.model.Note
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface NoteRepository : JpaRepository<Note, Long> {
 }
